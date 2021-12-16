edbd309cbd7f0b7b0561b161dcd8bc4775b0d225
milan <tripathimilan28@gmail.com>
2021-12-16 18:37:00 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "tripathimilan28@gmail.com"n
+echo "\
+[general]\n\
+email = \"andrewng_@hotmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
